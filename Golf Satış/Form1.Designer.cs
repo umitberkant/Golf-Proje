8fd8a4691d973bf3d20afabf5543e5044e30c78a
umitberkant16@gmail.com <umitberkant16@gmail.com>
2021-01-27 23:32:19 +0300

    Add project files.

---
+++ Golf Satış/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Golf_Satış
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.san1 = new System.Windows.Forms.RadioButton();
+            this.san2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mtr1 = new System.Windows.Forms.RadioButton();
+            this.mtr2 = new System.Windows.Forms.RadioButton();
+            this.d1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.d2 = new System.Windows.Forms.RadioButton();
+            this.d3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ek1 = new System.Windows.Forms.CheckBox();
+            this.ek2 = new System.Windows.Forms.CheckBox();
+            this.ek3 = new System.Windows.Forms.CheckBox();
+            this.ek4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fiyatlabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ŞANZIMAN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // san1
+            // 
+            this.san1.AutoSize = true;
+            this.san1.Location = new System.Drawing.Point(104, 119);
+            this.san1.Name = "san1";
+            this.san1.Size = new System.Drawing.Size(101, 21);
+            this.san1.TabIndex = 1;
+            this.san1.TabStop = true;
+            this.san1.Text = "OTOMATİK";
+            this.san1.UseVisualStyleBackColor = true;
+            // 
+            // san2
+            // 
+            this.san2.AutoSize = true;
+            this.san2.Location = new System.Drawing.Point(104, 168);
+            this.san2.Name = "san2";
+            this.san2.Size = new System.Drawing.Size(86, 21);
+            this.san2.TabIndex = 2;
+            this.san2.TabStop = true;
+            this.san2.Text = "MANUEL";
+            this.san2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MOTOR";
+            // 
+            // mtr1
+            // 
+            this.mtr1.AutoSize = true;
+            this.mtr1.Location = new System.Drawing.Point(22, 33);
+            this.mtr1.Name = "mtr1";
+            this.mtr1.Size = new System.Drawing.Size(74, 21);
+            this.mtr1.TabIndex = 4;
+            this.mtr1.TabStop = true;
+            this.mtr1.Text = "1.0 TSİ";
+            this.mtr1.UseVisualStyleBackColor = true;
+            // 
+            // mtr2
+            // 
+            this.mtr2.AutoSize = true;
+            this.mtr2.Location = new System.Drawing.Point(22, 82);
+            this.mtr2.Name = "mtr2";
+            this.mtr2.Size = new System.Drawing.Size(75, 21);
+            this.mtr2.TabIndex = 5;
+            this.mtr2.TabStop = true;
+            this.mtr2.Text = "1.6 TDİ";
+            this.mtr2.UseVisualStyleBackColor = true;
+            // 
+            // d1
+            // 
+            this.d1.AutoSize = true;
+            this.d1.Location = new System.Drawing.Point(15, 21);
+            this.d1.Name = "d1";
+            this.d1.Size = new System.Drawing.Size(83, 21);
+            this.d1.TabIndex = 6;
+            this.d1.TabStop = true;
+            this.d1.Text = "MİDLİNE";
+            this.d1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(444, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DONANIM";
+            // 
+            // d2
+            // 
+            this.d2.AutoSize = true;
+            this.d2.Location = new System.Drawing.Point(15, 70);
+            this.d2.Name = "d2";
+            this.d2.Size = new System.Drawing.Size(119, 21);
+            this.d2.TabIndex = 8;
+            this.d2.TabStop = true;
+            this.d2.Text = "COMFORLİNE";
+            this.d2.UseVisualStyleBackColor = true;
+            // 
+            // d3
+            // 
+            this.d3.AutoSize = true;
+            this.d3.Location = new System.Drawing.Point(15, 118);
+            this.d3.Name = "d3";
+            this.d3.Size = new System.Drawing.Size(93, 21);
+            this.d3.TabIndex = 9;
+            this.d3.TabStop = true;
+            this.d3.Text = "HİGHLİNE";
+            this.d3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.mtr1);
+            this.groupBox1.Controls.Add(this.mtr2);
+            this.groupBox1.Location = new System.Drawing.Point(281, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(148, 115);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.d1);
+            this.groupBox2.Controls.Add(this.d2);
+            this.groupBox2.Controls.Add(this.d3);
+            this.groupBox2.Location = new System.Drawing.Point(447, 120);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(181, 151);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // ek1
+            // 
+            this.ek1.AutoSize = true;
+            this.ek1.Location = new System.Drawing.Point(681, 120);
+            this.ek1.Name = "ek1";
+            this.ek1.Size = new System.Drawing.Size(120, 21);
+            this.ek1.TabIndex = 12;
+            this.ek1.Text = "ACC (2500TL)";
+            this.ek1.UseVisualStyleBackColor = true;
+            // 
+            // ek2
+            // 
+            this.ek2.AutoSize = true;
+            this.ek2.Location = new System.Drawing.Point(681, 169);
+            this.ek2.Name = "ek2";
+            this.ek2.Size = new System.Drawing.Size(241, 21);
+            this.ek2.TabIndex = 13;
+            this.ek2.Text = "ŞERİT TAKİP ASİSTANI (4000TL)";
+            this.ek2.UseVisualStyleBackColor = true;
+            // 
+            // ek3
+            // 
+            this.ek3.AutoSize = true;
+            this.ek3.Location = new System.Drawing.Point(681, 222);
+            this.ek3.Name = "ek3";
+            this.ek3.Size = new System.Drawing.Size(172, 21);
+            this.ek3.TabIndex = 14;
+            this.ek3.Text = "CAM TAVAN (3500TL)";
+            this.ek3.UseVisualStyleBackColor = true;
+            // 
+            // ek4
+            // 
+            this.ek4.AutoSize = true;
+            this.ek4.Location = new System.Drawing.Point(681, 272);
+            this.ek4.Name = "ek4";
+            this.ek4.Size = new System.Drawing.Size(344, 21);
+            this.ek4.TabIndex = 15;
+            this.ek4.Text = "TÜRKİYEDE YAŞIYORUM (%45 ÖTV    %18 KDV)";
+            this.ek4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(521, 198);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fiyatlabel
+            // 
+            this.fiyatlabel.AutoSize = true;
+            this.fiyatlabel.Location = new System.Drawing.Point(752, 404);
+            this.fiyatlabel.Name = "fiyatlabel";
+            this.fiyatlabel.Size = new System.Drawing.Size(46, 17);
+            this.fiyatlabel.TabIndex = 17;
+            this.fiyatlabel.Text = "FİYAT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 623);
+            this.Controls.Add(this.fiyatlabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ek4);
+            this.Controls.Add(this.ek3);
+            this.Controls.Add(this.ek2);
+            this.Controls.Add(this.ek1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.san2);
+            this.Controls.Add(this.san1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton san1;
+        private System.Windows.Forms.RadioButton san2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton mtr1;
+        private System.Windows.Forms.RadioButton mtr2;
+        private System.Windows.Forms.RadioButton d1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton d2;
+        private System.Windows.Forms.RadioButton d3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox ek1;
+        private System.Windows.Forms.CheckBox ek2;
+        private System.Windows.Forms.CheckBox ek3;
+        private System.Windows.Forms.CheckBox ek4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label fiyatlabel;
+    }
+}
+
---
+++ Golf Satış/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Golf_Satış
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int otv = 0;
+            int kdv = 0;
+            int t = 0;
+
+            int m10 = 105000;
+            int c10 = 125000;
+            int h10 = 180000;
+
+            int m16 = 125000;
+            int c16 = 145000;
+            int h16 = 200000;
+
+            if (mtr1.Checked && d1.Checked)
+            {
+                t = m10;           
+            if (ek1.Checked)
+            {
+                t = t + 2500;
+            }
+            if (ek2.Checked)
+            {
+                t = t + 4000;
+            }
+            if (ek3.Checked)
+            {
+                t = t + 3500;
+            }
+            if (ek4.Checked)
+            {
+                otv = t * 145 / 100;
+                kdv = t * 18 / 100;
+                t = t + otv + kdv;
+            }
+            if (san1.Checked)
+            {
+                t = t + 20000;
+            }
+            if (san2.Checked)
+            {
+                t = t + 10000;
+            }
+            fiyatlabel.Text = Convert.ToString(t);
+            }
+            if (mtr1.Checked && d2.Checked)
+            {
+                t = c10;
+                if (ek1.Checked)
+                {
+                    t = t + 2500;
+                }
+                if (ek2.Checked)
+                {
+                    t = t + 4000;
+                }
+                if (ek3.Checked)
+                {
+                    t = t + 3500;
+                }
+                if (ek4.Checked)
+                {
+                    otv = t * 145 / 100;
+                    kdv = t * 18 / 100;
+                    t = t + otv + kdv;
+                }
+                if (san1.Checked)
+                {
+                    t = t + 20000;
+                }
+                if (san2.Checked)
+                {
+                    t = t + 10000;
+                }
+                fiyatlabel.Text = Convert.ToString(t);
+            }
+            if (mtr1.Checked && d3.Checked)
+            {
+                t = h10;
+                if (ek1.Checked)
+                {
+                    t = t + 2500;
+                }
+                if (ek2.Checked)
+                {
+                    t = t + 4000;
+                }
+                if (ek3.Checked)
+                {
+                    t = t + 3500;
+                }
+                if (ek4.Checked)
+                {
+                    otv = t * 145 / 100;
+                    kdv = t * 18 / 100;
+                    t = t + otv + kdv;
+                }
+                if (san1.Checked)
+                {
+                    t = t + 20000;
+                }
+                if (san2.Checked)
+                {
+                    t = t + 10000;
+                }
+                fiyatlabel.Text = Convert.ToString(t);
+            }
+            if (mtr2.Checked && d1.Checked)
+            {
+                t = m16;
+                if (ek1.Checked)
+                {
+                    t = t + 2500;
+                }
+                if (ek2.Checked)
+                {
+                    t = t + 4000;
+                }
+                if (ek3.Checked)
+                {
+                    t = t + 3500;
+                }
+                if (ek4.Checked)
+                {
+                    otv = t * 145 / 100;
+                    kdv = t * 18 / 100;
+                    t = t + otv + kdv;
+                }
+                if (san1.Checked)
+                {
+                    t = t + 20000;
+                }
+                if (san2.Checked)
+                {
+                    t = t + 10000;
+                }
+                fiyatlabel.Text = Convert.ToString(t);
+            }
+            if (mtr2.Checked && d2.Checked)
+            {
+                t = c16;
+                if (ek1.Checked)
+                {
+                    t = t + 2500;
+                }
+                if (ek2.Checked)
+                {
+                    t = t + 4000;
+                }
+                if (ek3.Checked)
+                {
+                    t = t + 3500;
+                }
+                if (ek4.Checked)
+                {
+                    otv = t * 145 / 100;
+                    kdv = t * 18 / 100;
+                    t = t + otv + kdv;
+                }
+                if (san1.Checked)
+                {
+                    t = t + 20000;
+                }
+                if (san2.Checked)
+                {
+                    t = t + 10000;
+                }
+                fiyatlabel.Text = Convert.ToString(t);
+            }
+            if (mtr2.Checked && d3.Checked)
+            {
+                t = h16;
+                if (ek1.Checked)
+                {
+                    t = t + 2500;
+                }
+                if (ek2.Checked)
+                {
+                    t = t + 4000;
+                }
+                if (ek3.Checked)
+                {
+                    t = t + 3500;
+                }
+                if (ek4.Checked)
+                {
+                    otv = t * 145 / 100;
+                    kdv = t * 18 / 100;
+                    t = t + otv + kdv;
+                }
+                if (san1.Checked)
+                {
+                    t = t + 20000;
+                }
+                if (san2.Checked)
+                {
+                    t = t + 10000;
+                }
+                fiyatlabel.Text = Convert.ToString(t);
+            }
+        }      
+    }
+}
